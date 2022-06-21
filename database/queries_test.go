1f5ff91d19476ef950c75f693f823716ca07f8af
Andrew Querol <andrew@querol.me>
2022-06-21 15:49:36 -0500

    Add in a `InsertMessage` test

--- database/queries_test.go
+++ database/queries_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestDatabase_InsertUser(t *testing.T) {
@@ -31,6 +32,31 @@ func TestDatabase_FindUser(t *testing.T) {
 	assert.NotNil(t, foundUser)
 	assert.Equal(t, user.UUID, foundUser.UUID)
 	assert.Equal(t, user.Username, foundUser.Username)
+}
+
+func TestDatabase_InsertMessage(t *testing.T) {
+	db := createTestDatabase(t)
+	sender := createTestUser(t, db, "test")
+	recipient := createTestUser(t, db, "test2")
+
+	newMessageUuid := uuid.New()
+	newMessageTime := time.Now()
+	message, err := db.InsertMessage(Message{
+		UUID:      newMessageUuid,
+		CreatedAt: newMessageTime,
+		Content:   "This is a test",
+		Sender:    sender,
+		Recipient: recipient,
+	})
+
+	assert.Nil(t, err)
+	assert.Equal(t, newMessageUuid, message.UUID)
+	assert.Equal(t, newMessageTime, message.CreatedAt)
+	assert.Equal(t, "This is a test", message.Content)
+	assert.Equal(t, sender.UUID, message.Sender.UUID)
+	assert.Equal(t, sender.Username, message.Sender.Username)
+	assert.Equal(t, recipient.UUID, message.Recipient.UUID)
+	assert.Equal(t, recipient.Username, message.Recipient.Username)
 }
 
 func createTestDatabase(t *testing.T) *Database {
