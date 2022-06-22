c92f43dd234ba1ba9781d186cdd53e41be6f3c35
Andrew Querol <andrew@querol.me>
2022-06-21 20:41:20 -0500

    Finish adding in happy path API tests

--- api/api_test.go
+++ api/api_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/winsock/gochat/database"
@@ -13,15 +14,60 @@ import (
 
 func TestWebAPI_CreateUser(t *testing.T) {
 	api := createTestAPI(t)
-
 	req := httptest.NewRequest(http.MethodGet, "/user/create?username=test", nil)
 	w := httptest.NewRecorder()
+
 	api.CreateUser(w, req)
 	user, err := api.db.FindUser("test")
+	var response database.User
+	decodeErr := json.NewDecoder(w.Result().Body).Decode(&response)
 
 	assert.Equal(t, http.StatusCreated, w.Result().StatusCode)
+	assert.Nil(t, decodeErr)
 	assert.Nil(t, err)
-	assert.Equal(t, "test", user.Username)
+	assert.Equal(t, *user, response)
+}
+
+func TestWebAPI_SendMessage(t *testing.T) {
+	api := createTestAPI(t)
+	_ = createTestUser(t, api, "sender")
+	recipient := createTestUser(t, api, "recipient")
+	timeBeforeSend := time.Now()
+	req := httptest.NewRequest(http.MethodGet, "/message/send?sender=sender&recipient=recipient&message=test", nil)
+	w := httptest.NewRecorder()
+
+	api.SendMessage(w, req)
+	messages, err := api.db.FindMessagesForUser(recipient, timeBeforeSend, 0, 100)
+	var response SendMessageResponse
+	decodeErr := json.NewDecoder(w.Result().Body).Decode(&response)
+
+	assert.Equal(t, http.StatusCreated, w.Result().StatusCode)
+	assert.Nil(t, decodeErr)
+	assert.Nil(t, err)
+	assert.Len(t, messages, 1)
+	assert.Equal(t, messages[0], response.Message)
+}
+
+func TestWebAPI_SearchMessages(t *testing.T) {
+	api := createTestAPI(t)
+	sender := createTestUser(t, api, "sender")
+	recipient := createTestUser(t, api, "recipient")
+	message := createTestMessage(t, api, "test message", sender, recipient)
+	req := httptest.NewRequest(http.MethodGet, "/message/search?recipient=recipient", nil)
+	w := httptest.NewRecorder()
+
+	api.SearchMessages(w, req)
+	messages, err := api.db.FindMessagesForUser(recipient, message.CreatedAt, 0, 100)
+	var response SearchResponse
+	decodeErr := json.NewDecoder(w.Result().Body).Decode(&response)
+
+	assert.Equal(t, http.StatusOK, w.Result().StatusCode)
+	assert.Nil(t, decodeErr)
+	assert.Nil(t, err)
+	assert.Equal(t, messages, response.Messages)
+	assert.Equal(t, uint64(1), response.Count)
+	assert.Equal(t, uint64(0), response.Offset)
+	assert.Equal(t, uint64(100), response.Limit)
 }
 
 func createTestAPI(t *testing.T) *WebAPI {
